9c1ad2029ac2f7bae31a081c11adc3a6c03cbeae
Carlos Duarte do Nascimento (Chester) <cd@pobox.com>
2010-06-02 21:28:05 -0700

    Removed COM dependency (now using swftools)

--- slidememe.php
+++ slidememe.php
@@ -1,44 +1,58 @@
 <?php
 
-    $CONVERT = '"C:\\Program Files\\ImageMagick-6.6.0-Q16\\convert.exe"';
-    $URL_PREFIX = "http://img.slide.memethis.com/";
+    // Paths to the external tools (convert from ImageMagick, swfrender from swftools & wget)
+    $CONVERT = '~/tools/convert';
+    $SWFRENDER = '~/tools/swfrender';
+    $WGET = '/usr/bin/wget';
     
+    // Directory to store temporary swf and image files
+    $TEMP_DIR = '...';
+    
+    // Site configuration
+    $GIF_LAST_PICTURE = '...';
+    $FINAL_DIR = "...";
+    $URL_PREFIX = ".../";
+    
+    // Just playing it safe...
     $slideshowUrl = $_REQUEST["url"];
     if (strpos($slideshowUrl, "http://www.slideshare.net/")!==0) {
         die("Invalid parameter");
     }
 
+    // Get the title and the slides list, with a little help from YQL
     $yql_title="http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'".$slideshowUrl."'%20and%20xpath%3D'%2F%2Fhead%2F%2Ftitle'&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
     $yql_slides="http://query.yahooapis.com/v1/public/yql?q=use%20%22http%3A%2F%2Fslide.memethis.com%2Fslideshare.slides.xml%22%3B%20select%20*%20from%20slideshare.slides%20where%20url%3D'".$slideshowUrl."'%3B&format=json";
     $slides_metainfo = json_decode(file_get_contents($yql_slides));
     $title = json_decode(file_get_contents($yql_title))->query->results->title;
  
-    $tempprefix = "img/img".uniqid();
+    $tempprefix = $TEMP_DIR . "img" . uniqid();
     $i = 1;
     foreach ($slides_metainfo->query->results->item->Show->Slide as $slide) {
-        $filename = $tempprefix.$i.".gif";
-        $ch = curl_init("http://localhost/swf2gif.asp?url=" . $slide->Src);
-	$fp = fopen($filename, "w");
-	curl_setopt($ch, CURLOPT_FILE, $fp);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_exec($ch);
-	curl_close($ch);
-	fclose($fp);
-        $i++;
-        if ($i>5) break;
+        // Let's download each slide's swf and convert it to a png
+        // (we'd rather use a GIF, but that's what swfrender can do)
+        $swf_name = $tempprefix . $i . ".swf";
+        $png_name = $tempprefix . $i . ".png";
+        exec($WGET . ' -O ' . $swf_name . ' ' . $slide->Src);
+        exec($SWFRENDER . ' -o ' . $png_name . ' ' . $swf_name);
+        // 5 slides limit
+        if (++$i > 5) {
+            break;
+        }
     }
     
-    $cmd = $CONVERT." -delay 200 -loop 0 ".$tempprefix."*.gif img/end.gif ".$tempprefix.".gif";
-    //shell_exec($cmd);
-    $WshShell = new COM("WScript.Shell");
-    $output = $WshShell->Exec($cmd)->StdOut->ReadAll;
+    // Now we grab the 5 PNGs and the last picture, resize and animate to a gif
+    $final_gif_name = "img" . uniqid() . ".gif";
+    exec($CONVERT . " -resize 320x240 -delay 200 -loop 0 " . $tempprefix . "*.png "
+                  . $GIF_LAST_PICTURE .  " " . $FINAL_DIR . $final_gif_name);
 
+    // Cleanup
     for($i=1; $i<=5; $i++) {
-        unlink($tempprefix.$i.".gif");
+        unlink($tempprefix . $i . ".swf");
+        unlink($tempprefix . $i . ".png");
     }
 
     $result_arr = array(
-        "gif" => $URL_PREFIX.$tempprefix.'.gif',
+        "gif" => $URL_PREFIX . $final_gif_name,
         "title" => $title,
         "url" => $slideshowUrl);
                     
